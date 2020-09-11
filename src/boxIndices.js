f32ab4220a5dcb6d567e5f0ff180b18bd770b4cb
razh <razh@users.noreply.github.com>
2020-09-11 03:31:03 -0400

    Use raw values for box-indices

--- src/boxIndices.js
+++ src/boxIndices.js
@@ -10,28 +10,28 @@ export var nx_ny_nz = [6];
 export var nx_ny_pz = [7];
 
 // Edges.
-export var px_py = [...px_py_pz, ...px_py_nz];
-export var px_ny = [...px_ny_pz, ...px_ny_nz];
-export var nx_py = [...nx_py_nz, ...nx_py_pz];
-export var nx_ny = [...nx_ny_nz, ...nx_ny_pz];
+export var px_py = [0, 1];
+export var px_ny = [2, 3];
+export var nx_py = [4, 5];
+export var nx_ny = [6, 7];
 
-export var px_pz = [...px_py_pz, ...px_ny_pz];
-export var px_nz = [...px_py_nz, ...px_ny_nz];
-export var nx_nz = [...nx_py_nz, ...nx_ny_nz];
-export var nx_pz = [...nx_py_pz, ...nx_ny_pz];
+export var px_pz = [0, 2];
+export var px_nz = [1, 3];
+export var nx_nz = [4, 6];
+export var nx_pz = [5, 7];
 
-export var py_pz = [...px_py_pz, ...nx_py_pz];
-export var py_nz = [...px_py_nz, ...nx_py_nz];
-export var ny_pz = [...px_ny_pz, ...nx_ny_pz];
-export var ny_nz = [...px_ny_nz, ...nx_ny_nz];
+export var py_pz = [0, 5];
+export var py_nz = [1, 4];
+export var ny_pz = [2, 7];
+export var ny_nz = [3, 6];
 
 // Faces.
-export var px = [...px_py, ...px_ny];
-export var nx = [...nx_py, ...nx_ny];
-export var py = [...px_py, ...nx_py];
-export var ny = [...px_ny, ...nx_ny];
-export var pz = [...px_pz, ...nx_pz];
-export var nz = [...px_nz, ...nx_nz];
+export var px = [0, 1, 2, 3];
+export var nx = [4, 5, 6, 7];
+export var py = [0, 1, 4, 5];
+export var ny = [2, 3, 6, 7];
+export var pz = [0, 2, 5, 7];
+export var nz = [1, 3, 4, 6];
 
 // All vertices.
-export var all = [...px, ...nx];
+export var all = [0, 1, 2, 3, 4, 5, 6, 7];
